5274ec6e51f49120dc9a2f13e10df24feb64b42d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2023-12-08 08:39:19 +0530

    sm8250-common: rootdir: Cleanup
    * Remove unused or disabled shell scripts
    * Remove init.qcom.factory.rc, it is used for debugging
    
    Change-Id: I5495fccd63eb857e3cd7101c17809aa95f9e0b9c

--- edo.mk
+++ edo.mk
@@ -195,10 +195,7 @@ PRODUCT_PACKAGES_DEBUG += \
 PRODUCT_PACKAGES += \
     init_thermal.sh \
     init.class_main.sh \
-    init.crda.sh \
-    init.mdm.sh \
     init.qcom.early_boot.sh \
-    init.qcom.factory.rc \
     init.qcom.post_boot.sh \
     init.qcom.rc \
     init.qcom.sensors.sh \
