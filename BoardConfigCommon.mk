48fc8ce4250bff1b3236876a3ab40ae80a136553
ahnet-69 <syedmusicrealise@gmail.com>
2024-05-21 15:04:11 +0000

    sm8250-common: Fix device overriding manifest from extras repo
    Fixes the following:
    ERROR: files are incompatible: The following instances are in the device manifest but not specified in framework compatibility matrix:
        android.hardware.media.c2@1.1::IComponentStore/threesixtyra
        vendor.dolby.hardware.dms@2.0::IDms/default
    Suggested fix:
    1. Update deprecated HALs to the latest version.
    2. Check for any typos in device manifest or framework compatibility matrices with FCM version >= 5.
    3. For new platform HALs, add them to any framework compatibility matrix with FCM version >= 5 where applicable.
    4. For device-specific HALs, add to DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE or DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE.: Success
    INCOMPATIBLE

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -116,11 +116,11 @@ TARGET_GRALLOC_HANDLE_HAS_RESERVED_SIZE := true
 TARGET_FS_CONFIG_GEN := $(COMMON_PATH)/config.fs
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/sony/vintf/device_framework_matrix.xml
-DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE := $(COMMON_PATH)/manifest.xml
+DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
 ODM_MANIFEST_SKUS := ss ds
 ODM_MANIFEST_SS_FILES := $(COMMON_PATH)/manifest_ss.xml
