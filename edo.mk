cf5a9ad526a7b890ae80c39579968426ca5efd75
John Galt <johngaltfirstrun@gmail.com>
2025-09-08 13:16:35 -0400

    sm8250-common: begin building with Lindroid
    Since we have full device support, and the out of box experience is
    getting very good for advanced users.

--- edo.mk
+++ edo.mk
@@ -290,6 +290,9 @@ $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
 
+# Lindroid
+TARGET_BUILD_LINDROID := true
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
