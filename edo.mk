555e0d556ee0d2242a2286ee6f8a236f798b5404
unins0 <unins0000@proton.me>
2025-11-05 09:40:13 +0300

    voltage-ify

--- edo.mk
+++ edo.mk
@@ -34,7 +34,7 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-voltage
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
