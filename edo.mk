a19f6bca3d78ccea7cedac3225b4f66b9da377e7
unins0 <unins0000@proton.me>
2025-11-08 13:33:18 +0300

    Reapply "sm8250-common: Build some VNDK libraries"
    This reverts commit 40edb600946d924a353c5cbcdce7c96af45d82af.

--- edo.mk
+++ edo.mk
@@ -118,6 +118,7 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
+    libaudioroute.vendor \
     android.hardware.audio@6.0-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
@@ -131,7 +132,8 @@ PRODUCT_PACKAGES += \
     audio.primary.kona \
     audio.r_submix.default \
     audio.usb.default \
-    sound_trigger.primary.kona
+    sound_trigger.primary.kona \
+    libprocessgroup.vendor
 
 PRODUCT_PACKAGES += \
     liba2dpoffload \
